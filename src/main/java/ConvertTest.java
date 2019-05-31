99b2419075b0cdee76ac96b4e3e727ed64820e96
tomaszlach <tomaszlach86@gmail.com>
2019-05-31 10:02:18 +0200

    final polish

--- src/main/java/ConvertTest.java
+++ src/main/java/ConvertTest.java
@@ -4,7 +4,7 @@ public class ConvertTest {
 
     public static void main(String[] args) {
         for (int i = 1; i < 8; i++) {
-            System.out.format("%d dzień tygodnia to %s \n",i, CalendarConverter.convertDayToString(i));
+            System.out.format("%d dzień tygodnia to %s \n", i, CalendarConverter.convertDayToString(i));
         }
     }
 }
